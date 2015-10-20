a593f3900d67e2e9ecae0306bf0f9c75ce9553ad
Evan Hildreth <me@eph.me>
2015-10-19 21:29:07 -0400

    Adding tag cloud support
    With more on the way! hashtag-whack-a-mole

--- js/bsfixes.js
+++ js/bsfixes.js
@@ -2,5 +2,5 @@ jQuery( document ).ready(function( $ ) {
 	//would like to do this via mixins later, but for now we're hacking
     $('div.widget.panel ul').addClass("list-group");
 	$('div.widget.panel li').addClass("list-group-item");
-	$('div.widget.panel p, div.widget.panel form').wrap( "<div class='panel-body' />");
+	$('div.widget.panel p, div.widget.panel form, div.widget.panel div.tagcloud').wrap( "<div class='panel-body' />");
 });
